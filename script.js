ba628c7619b77c847079051f6267fd675639f047
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-30 18:58:01 -0300

    1º, 2º e 3º Requisito

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttonGenerator = document.getElementById('criar-carta');
+const cartGenerated = document.getElementById('carta-gerada');
+const inputText = document.getElementById('carta-texto');
+
+const addButtonGenerator = () => {
+  cartGenerated.innerHTML = '';
+  const values = inputText.value.split(' ');
+  for (let i = 0; i < values.length; i += 1) {
+    const span = document.createElement('span');
+    span.innerHTML = values[i];
+    cartGenerated.appendChild(span);
+  }
+  inputText.value = '';
+}
+
+buttonGenerator.addEventListener('click', addButtonGenerator);
