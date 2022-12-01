515b6676cc791905a815c2d24dff5b3e4af31ad4
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-01 14:21:19 -0300

    18º Requisito

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonGenerator = document.getElementById('criar-carta');
 const cartGenerated = document.getElementById('carta-gerada');
 const inputText = document.getElementById('carta-texto');
+const contador = document.getElementById('carta-contador');
 const sty = ['newspaper', 'magazine2', 'magazine1'];
 const wid = ['medium', 'big', 'reallybig'];
 const rot = ['rotateright', 'rotateleft'];
@@ -9,6 +10,7 @@ const inc = ['skewleft', 'skewright'];
 const mathRound = (classe) => classe[Math.floor(Math.random() * classe.length)];
 
 const addButtonGenerator = () => {
+  let count = 0;
   cartGenerated.innerHTML = '';
   if (inputText.value.length === 0 || inputText.value === ' ') {
     cartGenerated.innerHTML = 'Por favor, digite o conteúdo da carta.';
@@ -17,6 +19,7 @@ const addButtonGenerator = () => {
     for (let i = 0; i < values.length; i += 1) {
       const span = document.createElement('span');
       span.innerHTML = values[i];
+      count += 1;
       span.className = `${mathRound(sty)} ${mathRound(wid)} ${mathRound(rot)} ${mathRound(inc)}`;
       span.addEventListener('click', (e) => {
         e.target
@@ -24,7 +27,7 @@ const addButtonGenerator = () => {
       });
       cartGenerated.appendChild(span);
     }
-  }
+  } contador.innerHTML = count;
 };
 
 buttonGenerator.addEventListener('click', addButtonGenerator);
