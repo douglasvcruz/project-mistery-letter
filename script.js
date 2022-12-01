c29514ba3332378221f98d5c07e9369aef3e8531
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-01 01:10:13 -0300

    5º Requisito

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const inputText = document.getElementById('carta-texto');
 
 const addButtonGenerator = () => {
   cartGenerated.innerHTML = '';
+  if (inputText.innerHTML.length === 0) {
+    cartGenerated.innerHTML = 'Por favor, digite o conteúdo da carta.'
+  }
   const values = inputText.value.split(' ');
   for (let i = 0; i < values.length; i += 1) {
     const span = document.createElement('span');
