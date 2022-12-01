68d4851d452dfa5160eaf448a2a81ce1d9d78223
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-01 14:08:36 -0300

    17º Requisito

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const buttonGenerator = document.getElementById('criar-carta');
 const cartGenerated = document.getElementById('carta-gerada');
 const inputText = document.getElementById('carta-texto');
+const sty = ['newspaper', 'magazine2', 'magazine1'];
+const wid = ['medium', 'big', 'reallybig'];
+const rot = ['rotateright', 'rotateleft'];
+const inc = ['skewleft', 'skewright'];
+
+const mathRound = (classe) => classe[Math.floor(Math.random() * classe.length)];
 
 const addButtonGenerator = () => {
-  const classes = ['newspaper', 'magazine2', 'magazine1', 'medium', 'big',
-  'reallybig', 'rotateright', 'rotateleft', 'skewleft', 'skewright'];
   cartGenerated.innerHTML = '';
   if (inputText.value.length === 0 || inputText.value === ' ') {
     cartGenerated.innerHTML = 'Por favor, digite o conteúdo da carta.';
@@ -13,7 +17,11 @@ const addButtonGenerator = () => {
     for (let i = 0; i < values.length; i += 1) {
       const span = document.createElement('span');
       span.innerHTML = values[i];
-      span.className = classes[Math.floor(Math.random() * classes.length)]
+      span.className = `${mathRound(sty)} ${mathRound(wid)} ${mathRound(rot)} ${mathRound(inc)}`;
+      span.addEventListener('click', (e) => {
+        e.target
+          .className = `${mathRound(sty)} ${mathRound(wid)} ${mathRound(rot)} ${mathRound(inc)}`;
+      });
       cartGenerated.appendChild(span);
     }
   }
